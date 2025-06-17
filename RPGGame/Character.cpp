9f809646eeb5cbd0256e9bec3ea1afc5af362ee4
USSA <ussa9905@gmail.com>
2025-06-17 15:08:43 +0900

    레벨업 함수 추가

--- RPGGame/Character.cpp
+++ RPGGame/Character.cpp
@@ -22,7 +22,20 @@ void Character::displayStat()
 
 void Character::levelUp()
 {
+	if (level >= 10) {
+		cout << "최대 레벨에 도달했습니다!" << endl;
+		return;
+	}
 
+	if (exp >= 100) {
+		level++;
+		exp -= 100;
+
+		maxHealth += 20; // 최대 체력 증가
+		health = maxHealth; // 체력 회복
+
+		attack += 5; // 공격력 증가
+	}
 }
 
 void Character::useItem(int itemIndex)
