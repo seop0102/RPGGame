bf689e6a5b3699269476c6a88c5398d4c7d11645
USSA <ussa9905@gmail.com>
2025-06-17 14:59:33 +0900

    캐릭터 기본 클래스

--- RPGGame/Character.cpp
+++ RPGGame/Character.cpp
@@ -1,1 +1,31 @@
 #include "Character.h"
+
+Character::Character(string name)
+{
+	this->name = name;
+	level = 1;
+	health = 200;
+	maxHealth = 200;
+	attack = 30;
+	exp = 0;
+
+}
+
+void Character::displayStat()
+{
+	cout << "이름: " << name << endl;
+	cout << "레벨: " << name << endl;
+	cout << "체력: " << health << "/" << maxHealth << endl;
+	cout << "공격력: " << attack << endl;
+	cout << "경험치: " << exp << endl;
+}
+
+void Character::levelUp()
+{
+
+}
+
+void Character::useItem(int itemIndex)
+{
+
+}
--- RPGGame/Character.h
+++ RPGGame/Character.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include<string>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+class Item;
+
+class Character {
+
+private:
+	string name;
+	int level;
+	int health;
+	int maxHealth;
+	int attack;
+	int exp;
+	int gold;
+
+	vector<Item> inventory;
+
+public:
+	Character(string name);
+
+	void displayStat();
+	void levelUp();
+	void useItem(int itemIndex);
+
+};
