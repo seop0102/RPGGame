c3f4d3a298c8c9655f8c1aa44eb66eadd0070804
space-Line <rlatjsdn553@naver.com>
2025-06-18 17:52:27 +0900

    몬스터 인터페이스, 기초 틀
    데이터 테이블 정해지면 상세설정

--- RPGGame/Goblin.cpp
+++ RPGGame/Goblin.cpp
@@ -1,2 +1,14 @@
 #include "Goblin.h"
 #include "Monster.h"
+
+Monster01::Monster01(int level) {
+    name = "Monster01";
+    health = 40 + (level * 10);
+    attack = 17 + (level * 3);
+}
+
+std::string Monster01::getName() const { return name; }
+int Monster01::getHealth() const { return health; }
+int Monster01::getAttack() const { return attack; }
+
+void Monster01::takeDamage(int damage) { Monster::takeDamage(damage); }
--- RPGGame/Goblin.h
+++ RPGGame/Goblin.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Monster.h"
+
+//이름 바꿔야 함
+class Goblin : public Monster {
+public:
+    Goblin(int level);
+
+    std::string getName() const override;
+    int getHealth() const override;
+    int getAttack() const override;
+    void takeDamage(int damage) override;
+};
