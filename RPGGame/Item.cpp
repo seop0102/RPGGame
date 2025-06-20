b33974f42829c4b8472141e3c7f6110f8fbf1bd5
NonPlayer95 <130722174+Nonplayer95@users.noreply.github.com>
2025-06-20 23:44:44 +0900

    item 대대적수정, 몬스터 cpp일부수정
    몬스터 cpp의 경우, item이 추상클래스로 변해서 글자 몇개만 바꿧습니다.
    아이템은 튜터님말씀대로 추상클래스로 만들라하셔서 갈아엎엇음

--- RPGGame/Item.cpp
+++ RPGGame/Item.cpp
@@ -1,11 +1,13 @@
 #include "Item.h"
+#include<iostream>
+
 // 생성자
 Item::Item(const std::string& name, ItemType type, ItemTier tier,
-	int healthRecover = 0, int skillCharges = 0, int attackBonus = 0,
-	int defenseBonus = 0,int maxhealthBonus=0,int price)
-	:name(name), type(type), tier(tier), price(price),
-	healthRecover(healthRecover), skillCharges(skillCharges),
-	attackBonus(attackBonus), defenseBonus(defenseBonus), maxhealthBonus(maxhealthBonus), isEquipped(false) {}
+	int healthRecover, int skillCharges, int attackBonus,
+	int defenseBonus,int maxHealthBonus,int price)
+	:name(name), type(type), tier(tier), healthRecover(healthRecover), skillCharges(skillCharges),
+	attackBonus(attackBonus), defenseBonus(defenseBonus), maxHealthBonus(maxHealthBonus),
+	price(price), isEquipped(false) {}
 // getter 함수
 std::string Item::getName()const
 {
@@ -22,10 +24,6 @@ ItemType Item::getType() const
 ItemTier Item::getTier() const
 {
 	return tier;
-}
-int Item::getPrice() const
-{
-	return price;
 }
 int Item::getHealthRecover() const
 {
@@ -48,7 +46,12 @@ int Item::getSkillCharges() const
 
 int Item::getMaxHealthBonus()const
 {
-	return maxhealthBonus;
+	return maxHealthBonus;
+}
+
+int Item::getPrice() const
+{
+	return price;
 }
 
 bool Item::getIsEquipped() const 
@@ -109,21 +112,95 @@ std::string Item::getIteminfo() const
 	{
 		if (attackBonus > 0)info += "공격력 +" + std::to_string(attackBonus) + " ";
 		if (defenseBonus > 0)info += "방어력 +" + std::to_string(defenseBonus) + " ";
-		if (maxhealthBonus > 0)info += "최대 체력 +" + std::to_string(maxhealthBonus) + " ";
+		if (maxHealthBonus > 0)info += "최대 체력 +" + std::to_string(maxHealthBonus) + " ";
 	}
 
 	info += "- " + std::to_string(price) + " gold";
 	return info;
 }
-//사용 예시
-// 
-// Item healthPotion("체력 물약", EDIBLE, NORMAL,  100,       2,        0,     0,      1000)
-//                  (    이름   , 타입, 등급, 체력회복량,스킬사용횟수, 공격력, 방어력, 가격(골드))
-// 
+// Edible Item 상세
+EdibleItem::EdibleItem(const std::string& name,ItemTier tier,int healthRecover,int skilCharges,
+	int attackBonus,int price)
+	:Item(name,ItemType::EDIBLE,tier,healthRecover,skillCharges,attackBonus,0,0,price)
+	// tier 이후부터 각각 healthRecover, skillCharges, attackBonus, defenseBonus, maxHealthBonus, price
+	// 소모품은 방어력, 최대 체력 증가 없음!
+{ }
+void EdibleItem::use()
+{
+	std::cout << name << "을(를) 사용했습니다!" << std::endl;
+	// 실제 사용 효과는 게임매니저나 캐릭터에서 하셔야할듯..
+}
+std::string EdibleItem::getEffect()const
+{
+	std::string effect = "";
+	if (healthRecover > 0) effect += "체력 회복 +" + std::to_string(healthRecover) + " ";
+	if (skillCharges > 0) effect += "스킬 사용 횟수 +" + std::to_string(skillCharges) + " ";
+	if (attackBonus > 0) effect += "임시 공격력+" + std::to_string(attackBonus) + " ";
+	return effect;
+}
+bool EdibleItem::canUse()const
+{
+	return true; // 소모품은 항상 사용 가능하게끔
+}
+// Weapon Item 상세
+WeaponItem::WeaponItem(const std::string& name,ItemTier tier,int attackBonus,int price)
+	:Item(name,ItemType::WEAPON,tier,0,0,attackBonus,0,0,price)
+	// 무기는 체력 회복, 스킬사용횟수,방어력,최대체력 증가 없음!
+{ }
+void WeaponItem::use()
+{
+	if (!isEquipped)
+	{
+		isEquipped = true;
+		std::cout << name << "을(를) 장착했습니다!" << std::endl;
+	}
+	else
+	{
+		std::cout << name << "은(는) 이미 장착중입니다!" << std::endl;
+	}
+}
+std::string WeaponItem::getEffect()const
+{
+	return"공격력 +" + std::to_string(attackBonus);
+}
+bool WeaponItem::canUse()const
+{
+	return !isEquipped;//무기는 장착되지 않았을 때만 사용 가능
+}
+
+// Armor Item 상세
+ArmorItem::ArmorItem(const std::string& name,ItemTier tier,int defenseBonus,int maxHealthBonus,int price)
+	:Item(name,ItemType::ARMOR,tier,0,0,0,defenseBonus,maxHealthBonus,price)
+	// 방어구는 체력 회복, 스킬사용횟수,공격력 없음!
+{ }
+void ArmorItem::use()
+{
+	if (!isEquipped)
+	{
+		isEquipped = true;
+		std::cout << name << "을(를) 장착했습니다!" << std::endl;
+	}
+	else
+	{
+		std::cout << name << "은(는) 이미 장착중입니다!" << std::endl;
+	}
+}
+std::string ArmorItem::getEffect()const
+{
+	std::string effect = "";
+	if (defenseBonus > 0) effect += "방어력 +" + std::to_string(defenseBonus) + " ";
+	if (maxHealthBonus > 0) effect += "최대 체력+" + std::to_string(maxHealthBonus) + " ";
+	return effect;
+}
+bool ArmorItem::canUse() const
+{
+	return !isEquipped; //방어구는 장착되지 않았을 떄만 사용 가능
+}
 //
-//	//  순서: name, type, tier, hp, skill, att, def, price
-//	Item healthPotion("체력 물약", EDIBLE, NORMAL, 100, 2, 0, 0, 1100);
-//	Item attackPotion("공격력 강화", EDIBLE, NORMAL, 0, 0, 10, 0, 150);
-//	Item sword("철검", WEAPON, RARE, 0, 0, 15, 0, 2000);
-//	Item armor("가죽갑옷", ARMOR, NORMAL, 0, 0, 0, 8, 1500);
-//
+// 사용 예시
+// EdibleItem* healthPotion = new EdibleItem("체력 물약", ItemTier::NORMAL, 100, 2, 0, 1100)
+// WeaponItem* sword = new WeaponItem("철검", ItemTier::RARE, 15, 2000);
+// ArmorItem* lightArmor= nwe ArmorItem("가죽",ItemTier::EPIC,10,3,6000);
+// 
+// 
+
--- RPGGame/Item.h
+++ RPGGame/Item.h
@@ -16,7 +16,7 @@ enum class ItemTier // 아이템 등급
 };
 class Item
 {
-private:
+protected: // private>protected 로 해서 상속가능하게끔
 	std::string name;
 	std::string description;
 	ItemType type;
@@ -24,18 +24,26 @@ private:
 	int price;
 
 	//효과 관련(회복,공격력,방어력,스킬사용횟수, 장비 착용여부)
-	int healthRecover; // 회복물약 체력 회복
+	int healthRecover; // 회복물약의 체력 회복
 	int attackBonus;  // 공격력( 무기나 attack boost)
 	int defenseBonus;  //방어력
 	int skillCharges;   // 스킬 사용 횟수
-	int maxhealthBonus; // 최대 체력 증가
-	bool isEquipped;   // 장비장착여부
+	int maxHealthBonus; // 최대 체력 증가
+	bool isEquipped;   // 장비 장착 했는지 여부
 
 public:
 	//생성자
 	Item(const std::string& name, ItemType type, ItemTier tier,
 		int healthRecover = 0, int skillCharges = 0, int attackBonus = 0,
-		int defenseBonus = 0, int maxhealthBonus = 0, int price = 0);
+		int defenseBonus, int maxHealthBonus = 0, int price = 0);
+
+	// 가상 소멸자
+	virtual~Item() = default;
+
+	//순수가상함수(인터페이스)
+	virtual void use() = 0; // 아이템 사용
+	virtual std::string getEffect() const = 0; // 아이템 효과 설명
+	virtual bool canUse() const = 0; // 사용 가능 여부
 	
 	//getter 함수
 	std::string getName()const;
@@ -59,5 +67,41 @@ public:
 	std::string getTypeString()const;
 	std::string getTierString()const;
 	std::string getIteminfo()const; // 상점에서 보여줄 아이템 정보
-
 };
+//구체적인 아이템 클래스
+// 소모품
+class EdibleItem : public Item
+{
+public: // 소모품에서 방어력 증가와 최대 체력 증가 없음
+	EdibleItem(const std::string& name, ItemTier tier, int healthRecover, int skillCharges,
+		int attakBonus,int price);
+
+	//순수가상함수 부분
+	void use() override;
+	std::string getEffect() const override;
+	bool canUse() const override;
+};
+
+// 아이템-무기
+class WeaponItem :public Item
+{
+public:
+	WeaponItem(const std::string& name, ItemTier tier, int attackBonus, int price);
+
+	// 순수가상함수 부분
+	void use() override;
+	std::string getEffect()const override;
+	bool canUse() const override;
+};
+// 아이템-방어구
+class ArmorItem :public Item
+{
+public:
+	ArmorItem(const std::string& name, ItemTier tier, int defenseBonus, int maxHealthBonus, int price);
+
+	//순수가상함수 부분
+	void use() override;
+	std::string getEffect() const override;
+	bool canUse() const override;
+};
+
