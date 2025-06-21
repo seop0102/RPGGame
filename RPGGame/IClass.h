c34975aeef97c8f36ccc31e57b1bbc445af6470c
BYUN-HYUK <hyuk990504@gmail.com>
2025-06-21 18:46:04 +0900

    ICombatant.h / Rogue, Warrior, Archer 구현 / Utils (확률 주사위) 구현

--- RPGGame/IClass.h
+++ RPGGame/IClass.h
@@ -11,6 +11,7 @@ class Monster;
 enum class SkillType {
 	BASIC_ATTACK,
 
+// 1번 - 일반공격
 //도적 스킬
 ROGUE1, //찢기
 ROGUE2, //날렵한 손
@@ -35,14 +36,40 @@ WARRIOR4, //버티기
 class IClass {
 
 public:
+	
+	// 직업 별 고유 스킬 목록 반환 함수
 	virtual std::vector<SkillType> getActiveSkills() const = 0;
 	virtual void useSkill(SkillType skillType, Character& self, Monster& target) = 0;
+
+	// 패시브 스킬 함수
+	virtual void applyPassiveSkill(Character& self) = 0;
+	
+	// 직업 이름 반환
 	virtual std::string getClassName() const = 0;
+	
 	virtual ~IClass() = default;
 };
 
-class warrior : public IClass {
+class Warrior : public IClass {
 public:
-	
-	
+	std::string getClassName() const override { return "워리어"; }
+	std::vector<SkillType> getActiveSkills() const override;
+	void useSkill(SkillType skillType, Character& self, Monster& target);
+	void applyPassiveSkill(Character& self) override;	
+};
+
+class Archer : public IClass {
+public:
+	std::string getClassName() const override { return "궁수"; }
+	std::vector<SkillType> getActiveSkills() const override;
+	void useSkill(SkillType skillType, Character& self, Monster& target) override;
+	void applyPassiveSkill(Character& self) override;
+};
+
+class Rogue : public IClass {
+public:
+	std::string getClassName() const override { return "도적"; }
+	std::vector<SkillType> getActiveSkills() const override;
+	void useSkill(SkillType skillType, Character& self, Monster& target) override;
+	void applyPassiveSkill(Character& self) override;
 };
---
+++ RPGGame/Rogue.cpp
@@ -0,0 +1,80 @@
+#include "Rogue.h"
+#include "IClass.h"
+#include "Character.h"
+#include "Monster.h"
+#include "Utils.h"
+#include <iostream>
+
+namespace {
+    void performRogue1(Character& self, Monster& target); // 찢기
+    void performRogue2(Character& self, Monster& target); // 날렵한 손
+    void performRogue3(Character& self, Monster& target); // 급습
+    void performRogue4(Character& self, Monster& target); // 숨기
+}
+
+std::vector<SkillType> Rogue::getActiveSkills() const {
+    return { SkillType::ROGUE1, SkillType::ROGUE2, SkillType::ROGUE3, SkillType::ROGUE4 };
+}
+
+void Rogue::applyPassiveSkill(Character& self) {
+    // 전투 시작 시 회피율 보너스를 얻는 패시브
+    self.setDodgeChance(self.getDodgeChance() + 20);
+    std::cout << "[패시브 발동] " << self.getName() << "은(는) 날렵한 몸놀림으로 회피율이 20% 증가했습니다!" << std::endl;
+}
+
+void Rogue::useSkill(SkillType skillType, Character& self, Monster& target) {
+    switch (skillType) {
+    case SkillType::ROGUE1: performRogue1(self, target); break;
+    case SkillType::ROGUE2: performRogue2(self, target); break;
+    case SkillType::ROGUE3: performRogue3(self, target); break;
+    case SkillType::ROGUE4: performRogue4(self, target); break;
+    default:
+        std::cout << "도적은 사용할 수 없는 스킬입니다." << std::endl;
+        break;
+    }
+}
+
+namespace {
+    // ROGUE1: 찢기
+    void performRogue1(Character& self, Monster& target) {
+        std::cout << self.getName() << "의 찢기!" << std::endl;
+        int damage = self.getAttack() + 15;
+        std::cout << "날카로운 단검이 " << target.getName() << "에게 상처를 입혀 " << damage << "의 피해!" << std::endl;
+        target.takeDamage(damage);
+    }
+
+    // ROGUE2: 날렵한 손
+    void performRogue2(Character& self, Monster& target) {
+        std::cout << self.getName() << "의 날렵한 손!" << std::endl;
+        int stolenGold = Utils::getRandomInt(10, 30) * self.getLevel();
+        self.addGold(stolenGold); // Character의 골드 획득 함수 호출
+        std::cout << target.getName() << "에게서 " << stolenGold << " G를 훔쳤습니다!" << std::endl;
+
+        int damage = static_cast<int>(self.getAttack() * 0.5); // 훔치는 동안 약간의 피해
+        target.takeDamage(damage);
+    }
+
+    // ROGUE3: 급습
+    void performRogue3(Character& self, Monster& target) {
+        std::cout << self.getName() << "의 급습!" << std::endl;
+        // 치명타 확률이 높은 공격
+        int dice = Utils::rollDice();
+        int damage = 0;
+        if (dice >= 4) { // 주사위 4 이상일 때 치명타 (50% 확률)
+            damage = static_cast<int>(self.getAttack() * 2.0); // 200% 피해
+            std::cout << "치명타! " << target.getName() << "의 약점을 정확히 노렸습니다! " << damage << "의 피해!" << std::endl;
+        }
+        else {
+            damage = self.getAttack();
+            std::cout << "공격이 빗나가 약한 피해를 입혔습니다. " << damage << "의 피해." << std::endl;
+        }
+        target.takeDamage(damage);
+    }
+
+    // ROGUE4: 숨기
+    void performRogue4(Character& self, Monster& target) {
+        std::cout << self.getName() << "의 숨기!" << std::endl;
+        self.setIsHiding(true); // isHiding 플래그 활성화
+        std::cout << "그림자 속으로 사라집니다. 다음 턴 공격을 반드시 회피합니다." << std::endl;
+    }
+}
